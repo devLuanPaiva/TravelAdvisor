193584afbed06c4386e69989af01e8bdf7e3a117
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-10 17:17:57 -0300

    feat: create travel models interface

---
+++ core/src/travel/index.ts
@@ -0,0 +1,8 @@
+import { IDestination } from "./models/IDestination.interface";
+import { IPlace, PlaceType } from "./models/IPlace.interface";
+import {
+  ISearchFilters,
+  ISearchResult,
+} from "./models/ISearchResults.interface";
+
+export type { IDestination, IPlace, PlaceType, ISearchFilters, ISearchResult };
