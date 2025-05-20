5ad352f1ea502a2e3a634b7100ee4d243a6ef271
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-20 20:54:51 -0300

    feat: create auth session

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,13 @@
-export default function HomePage() {
+import {redirect} from 'next/navigation';
+import {getServerSession} from 'next-auth';
+export default async function HomePage() {
+    const session = await getServerSession();
+    if(!session) {
+        redirect('/authentication');
+    }
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Hello, {session.user?.name}!</h1>
             <p>Welcome to the home page!</p>
         </div>
     );
