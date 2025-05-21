66c3e8e4cd4147eda202297c58aea34233a9a6bc
Luan <devluanpaiva@gmail.com>
2025-05-21 08:04:12 -0300

    style: improve header style

--- frontend/src/components/template/Header.tsx
+++ frontend/src/components/template/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
-
 import { useEffect, useState } from "react";
+import { MdLocationOn } from "react-icons/md";
 
 interface HeaderProps {
   name: string;
@@ -36,9 +36,11 @@ export default function Header({ name }: Readonly<HeaderProps>) {
     );
   }, []);
   return (
-    <header className="flex items-center justify-between bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+    <header className="flex items-center justify-between bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white h-16 px-8 py-6">
       <h2>Olá, {name}!</h2>
-      <p>{city}</p>
+      <p className="flex items-center gap-1">
+        {city} <MdLocationOn />
+      </p>
     </header>
   );
 }
