a8ed86a7d465554e6cf82867d838a8b4690ae79d
Luan <devluanpaiva@gmail.com>
2025-05-22 13:13:34 -0300

    chore: configure session, callbacks and jwt

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,18 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import db from "@/lib/db/db";
 import NextAuth from "next-auth";
+import { v4 as uuid } from "uuid";
+import { schema } from "@/lib/schema";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { encode as defaultEncode } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { schema } from "@/lib/schema";
-import db from "@/lib/db/db";
+
+const adapter = PrismaAdapter(db);
 const handler = NextAuth({
-    adapter: PrismaAdapter(db),
+    adapter,
+    session: {
+        strategy: "jwt",
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -14,18 +21,16 @@ const handler = NextAuth({
         }),
         CredentialsProvider({
             credentials: {
-                email: {},
-                password: {},
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
             },
             authorize: async (credentials) => {
                 const validatedCredentials = schema.parse(credentials);
 
-                const user = await db.user.findFirst({
-                    where: {
-                        email: validatedCredentials.email,
-                        password: validatedCredentials.password,
-                    },
+                const user = await db.user.findUnique({
+                    where: { email: validatedCredentials.email }
                 });
+
 
                 if (!user) {
                     throw new Error("Invalid credentials.");
@@ -35,6 +40,38 @@ const handler = NextAuth({
             },
         }),
     ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account?.provider === "credentials") {
+                token.credentials = true;
+            }
+            return token;
+        },
+    },
+    jwt: {
+        encode: async function (params) {
+            if (params.token?.credentials) {
+                const sessionToken = uuid();
+
+                if (!params.token.sub) {
+                    throw new Error("No user ID found in token");
+                }
+
+                const createdSession = await adapter?.createSession?.({
+                    sessionToken: sessionToken,
+                    userId: params.token.sub,
+                    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                });
+
+                if (!createdSession) {
+                    throw new Error("Failed to create session");
+                }
+
+                return sessionToken;
+            }
+            return defaultEncode(params);
+        },
+    },
 });
 
 export { handler as GET, handler as POST };
