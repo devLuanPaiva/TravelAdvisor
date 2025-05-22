5fe67966568299d584babd2acdfcc9ec31adee4a
Luan <devluanpaiva@gmail.com>
2025-05-22 10:37:38 -0300

    chore: configure google and credentials providers in the route file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,40 @@
-import { handlers } from "@/lib/auth";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { schema } from "@/lib/schema";
+import db from "@/lib/db/db";
+const handler = NextAuth({
+    adapter: PrismaAdapter(db),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+            allowDangerousEmailAccountLinking: true,
+        }),
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                const validatedCredentials = schema.parse(credentials);
 
-export const { GET, POST } = handlers;
+                const user = await db.user.findFirst({
+                    where: {
+                        email: validatedCredentials.email,
+                        password: validatedCredentials.password,
+                    },
+                });
+
+                if (!user) {
+                    throw new Error("Invalid credentials.");
+                }
+
+                return user;
+            },
+        }),
+    ],
+});
+
+export { handler as GET, handler as POST };
