06ed4328c47b00ab94e6783aa933c2814dcf2894
Luan <devluanpaiva@gmail.com>
2025-05-23 07:14:23 -0300

    fix: change callbackUrl path

--- src/components/shared/Logout.tsx
+++ src/components/shared/Logout.tsx
@@ -4,7 +4,7 @@ import { RiLogoutCircleLine } from "react-icons/ri";
 export function Logout() {
   return (
     <button
-      onClick={() => signOut({ callbackUrl: "/authentication" })}
+      onClick={() => signOut({ callbackUrl: "/sign-in" })}
       className="fixed top-[90%] left-5 size-10  rounded-full bg-radial-[at_25%_25%] from-gray-900 via-gray-800 to-black z-50 flex items-center justify-center  text-white cursor-pointer"
     >
       <RiLogoutCircleLine size={30} />
