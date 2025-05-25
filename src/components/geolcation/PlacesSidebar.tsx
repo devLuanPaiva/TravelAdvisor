ed6b3c82abe1b03db5ab0776cf7b775670637867
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-25 09:17:20 -0300

    feat: add user session

--- src/components/geolcation/PlacesSidebar.tsx
+++ src/components/geolcation/PlacesSidebar.tsx
@@ -1,17 +1,31 @@
 'use client'
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
-import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowForward, IoIosArrowDown, IoIosLogOut } from "react-icons/io";
 
+const placeTypes = [
+    { label: 'Restaurantes', value: 'restaurant' },
+    { label: 'Hotéis', value: 'lodging' },
+    { label: 'Hospitais', value: 'hospital' },
+    { label: 'Mercados', value: 'supermarket' },
+    { label: 'Cafés', value: 'cafe' },
+    { label: 'Farmácias', value: 'pharmacy' },
+    { label: 'Bancos', value: 'bank' },
+    { label: 'Postos de Gasolina', value: 'gas_station' },
+    { label: 'Parques', value: 'park' },
+    { label: 'Museus', value: 'museum' },
+]
 export interface PlacesSidebarProps {
-    placeTypes: { label: string; value: string }[]
     places: google.maps.places.PlaceResult[]
     selectedType: string
     setSelectedType: (type: string) => void
     isShowSidebar: boolean
+    session: Session
 }
 
-export function PlacesSidebar({ placeTypes, places, selectedType, setSelectedType, isShowSidebar }: Readonly<PlacesSidebarProps>) {
+export function PlacesSidebar({ places, selectedType, setSelectedType, isShowSidebar, session }: Readonly<PlacesSidebarProps>) {
     const [isTypeOpen, setIsTypeOpen] = useState(false);
     const [showAllPlaces, setShowAllPlaces] = useState(false);
 
@@ -23,10 +37,10 @@ export function PlacesSidebar({ placeTypes, places, selectedType, setSelectedTyp
 
     return (
         <aside
-            className={`w-1/4 h-full bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white p-4 overflow-y-auto border-r border-gray-700 transition-all duration-300 ease-in-out ${isShowSidebar ? 'block' : 'hidden'
+            className={`relative w-1/4 h-full bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white overflow-y-auto border-r border-gray-700 transition-all duration-300 ease-in-out ${isShowSidebar ? 'block' : 'hidden'
                 }`}
         >
-            <h2 className="text-xl font-semibold mb-4">
+            <h2 className="text-xl font-semibold mb-4 p-4">
                 <button
                     className="w-full flex justify-between items-center text-base font-bold text-gray-200 hover:text-white transition-colors duration-200"
                     onClick={() => setIsTypeOpen(!isTypeOpen)}
@@ -41,13 +55,13 @@ export function PlacesSidebar({ placeTypes, places, selectedType, setSelectedTyp
             </h2>
 
             {isTypeOpen && (
-                <ul className="space-y-2 mb-6">
+                <ul className="space-y-2 mb-6 p-4">
                     {placeTypes.map((type) => (
                         <li key={type.value}>
                             <button
                                 className={`w-full text-left px-3 py-2 rounded-md transition-colors duration-200 ${selectedType === type.value
-                                        ? 'bg-blue-600 text-white'
-                                        : 'bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white'
+                                    ? 'bg-blue-600 text-white'
+                                    : 'bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white'
                                     }`}
                                 onClick={() => setSelectedType(type.value)}
                             >
@@ -58,8 +72,8 @@ export function PlacesSidebar({ placeTypes, places, selectedType, setSelectedTyp
                 </ul>
             )}
 
-            <h3 className="text-lg font-semibold mb-3 text-gray-100">Resultados próximos</h3>
-            <div className="space-y-4 flex flex-col items-center">
+            <h3 className="text-lg font-semibold mb-3 text-gray-100 p-4">Resultados próximos</h3>
+            <section className="space-y-4 flex flex-col items-center p-4">
                 {displayedPlaces.map((place, index) => (
                     <div
                         key={place.place_id ?? index}
@@ -97,7 +111,23 @@ export function PlacesSidebar({ placeTypes, places, selectedType, setSelectedTyp
                         Mostrar menos
                     </button>
                 )}
-            </div>
+            </section>
+
+            <section className="sticky bottom-0 z-20 w-full left-0 py-2 px-4 flex items-center justify-between gap-1 bg-gray-950">
+                <div className="flex items-center gap-2">
+                    <Image src={session.user?.image ?? ''} width={45} height={45} alt="usuário" className="rounded-[5px]" />
+                    <div className="flex flex-col">
+                        <h3 className="text-sm font-bold">Olá, {session.user?.name}!</h3>
+                        <p className="text-xs">{session.user?.email} </p>
+                    </div>
+                </div>
+                <button
+                    onClick={() => signOut({ callbackUrl: "/sign-in" })}
+                    className="size-10  rounded-full bg-radial-[at_25%_25%] from-gray-900 via-gray-800 to-black z-50 flex items-center justify-center  text-white cursor-pointer"
+                >
+                    <IoIosLogOut size={30} />
+                </button>
+            </section>
         </aside>
 
     );
