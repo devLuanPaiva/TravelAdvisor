e048fa62c3fc508db496982e803b5b96f582801d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-26 20:06:48 -0300

    test: create test to call signIn function on button click

--- __tests__/GoogleSignIn.test.tsx
+++ __tests__/GoogleSignIn.test.tsx
@@ -1,4 +1,5 @@
-;
+import { signIn } from "next-auth/react";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { GoogleSignIn } from "../src/components/auth/GoogleSigIn";
 import '@testing-library/jest-dom';
@@ -12,5 +13,13 @@ describe("GoogleSignIn Component", () => {
     it("render the button with correct text", () => {
         render(<GoogleSignIn />)
         expect(screen.getByRole("button", { name: /entrar com google/i })).toBeInTheDocument();
+    })
+    it("call signIn on button click", async () => {
+        const user = userEvent.setup()
+        render(<GoogleSignIn />)
+        const button = screen.getByRole("button", { name: /entrar com google/i });
+        await user.click(button);
+
+        expect(signIn).toHaveBeenCalledWith("google", { callbackUrl: "/home" });
     })
 })
