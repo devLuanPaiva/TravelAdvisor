2a1604d5a21c27a9d87a8b59cc52141eece34434
Luan <devluanpaiva@gmail.com>
2025-05-26 16:28:12 -0300

    chore: configure jest setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  preset: "ts-jest",
+};
+
+module.exports = createJestConfig(config);
