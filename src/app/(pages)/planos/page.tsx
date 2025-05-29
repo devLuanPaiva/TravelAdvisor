0a59a940a2d4a8338670c32b06ba77dd496d717b
Luan <devluanpaiva@gmail.com>
2025-05-29 09:06:09 -0300

    refactor: change page name

--- src/app/(pages)/planos/page.tsx
+++ src/app/(pages)/planos/page.tsx
@@ -2,10 +2,10 @@ import { Payment } from "@/components/payment/Payment";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export default async function PaymentPage() {
+export default async function PlainsPage() {
   const session = await getServerSession();
   if (!session) {
     redirect("/sign-in");
   }
-  return <Payment session={session}/>;
+  return <Payment session={session} />;
 }
