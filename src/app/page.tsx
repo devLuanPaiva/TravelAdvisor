75f9a183e7b741ec825d5f659c68054ee87c0fbe
Luan <devluanpaiva@gmail.com>
2025-05-30 07:35:05 -0300

    feat: add black logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,23 @@
-'use client';
-import {motion} from "framer-motion";
-import image1 from "@/assets/image1.jpg";
-import image2 from "@/assets/image2.jpg";
-import image3 from "@/assets/image3.jpg";
+import { Images } from "@/components/landing/Images";
+import logo from "@/assets/blackLogo.png";
 import { WellCome } from "@/components/landing/WellCome";
 import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex h-screen w-screen items-start justify-between bg-zinc-100">
-      <section className="relative w-[50%] h-full flex items-center justify-center">
-        <motion.div
-          initial={{ opacity: 0, y: -50, rotate: -15 }}
-          animate={{ opacity: 1, y: 0, rotate: -12 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          className="absolute top-28 right-56 z-10"
-        >
+      <section className="w-[50%] h-full flex flex-col items-center justify-between relative">
+        <div className="w-full  flex items-center justify-start p-3 px-5">
           <Image
-            src={image1}
-            alt="image1"
-            width={220}
-            height={300}
-            className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
+            src={logo}
+            alt="logo"
+            width={100}
+            height={100}
+            className="h-20 w-20"
           />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 50, rotate: 15 }}
-          animate={{ opacity: 1, y: 0, rotate: 6 }}
-          transition={{ duration: 0.8, delay: 0.4 }}
-          className="absolute top-48 right-5 z-20"
-        >
-          <Image
-            src={image2}
-            alt="image2"
-            width={220}
-            height={300}
-            className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8, delay: 0.6 }}
-          className="absolute bottom-40 right-72 z-0"
-        >
-          <Image
-            src={image3}
-            alt="image3"
-            width={220}
-            height={300}
-            className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
-          />
-        </motion.div>
+        </div>
+        <Images />
       </section>
-      <WellCome/>
+      <WellCome />
     </main>
   );
 }
