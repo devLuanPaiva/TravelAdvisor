340a4960e0cdeab2d885870eb1b5107bb729cd34
Luan <devluanpaiva@gmail.com>
2025-05-31 17:23:10 -0300

    style: adjust the size adaptation of images

--- src/components/landing/Images.tsx
+++ src/components/landing/Images.tsx
@@ -4,21 +4,21 @@ import image1 from "@/assets/image1.jpg";
 import image2 from "@/assets/image2.jpg";
 import image3 from "@/assets/image3.jpg";
 import Image from "next/image";
+
 export function Images() {
   return (
-    <section>
+    <section className="">
       <motion.div
         initial={{ opacity: 0, y: -50, rotate: -15 }}
         animate={{ opacity: 1, y: 0, rotate: -12 }}
         transition={{ duration: 0.8, delay: 0.2 }}
-        className=" absolute top-28 right-56 z-10"
+        className="absolute top-20 right-10 md:right-56 z-10 w-32 sm:w-40 md:w-52 lg:w-56"
       >
         <Image
           src={image1}
           alt="image1"
-          width={220}
-          height={300}
-          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
+          layout="responsive"
+          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300 h-auto"
         />
       </motion.div>
 
@@ -26,14 +26,13 @@ export function Images() {
         initial={{ opacity: 0, y: 50, rotate: 15 }}
         animate={{ opacity: 1, y: 0, rotate: 6 }}
         transition={{ duration: 0.8, delay: 0.4 }}
-        className="absolute top-48 right-5 z-20"
+        className="absolute top-40 right-2 md:right-5 z-20 w-32 sm:w-40 md:w-52 lg:w-56"
       >
         <Image
           src={image2}
           alt="image2"
-          width={220}
-          height={300}
-          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
+          layout="responsive"
+          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300 h-auto"
         />
       </motion.div>
 
@@ -41,14 +40,13 @@ export function Images() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8, delay: 0.6 }}
-        className="absolute bottom-32 right-72 z-0"
+        className="absolute bottom-20 right-10 md:right-72 z-0 w-32 sm:w-40 md:w-52 lg:w-56"
       >
         <Image
           src={image3}
           alt="image3"
-          width={220}
-          height={300}
-          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300"
+          layout="responsive"
+          className="rounded-xl drop-shadow-xl hover:scale-105 transition-transform duration-300 h-auto"
         />
       </motion.div>
     </section>
