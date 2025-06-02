c578de38d4ceb8556e95869403184c8517f1e592
Luan <devluanpaiva@gmail.com>
2025-06-02 09:16:13 -0300

    style: change background color

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../shared/Logo";
 
 export function Footer() {
   return (
-    <footer className="flex flex-col sm:flex-row items-center justify-between bg-gray-900 text-white min-h-[10vh] px-8 py-6 gap-5">
+    <footer className="flex flex-col sm:flex-row items-center justify-between bg-gray-950 text-white min-h-[10vh] px-8 py-6 gap-5">
       <Logo />
       <p>© 2025 - Todos os direitos reservados</p>
     </footer>
