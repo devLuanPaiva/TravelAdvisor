0e556b04f7b5d0ba179a0983890659b5fb4c29e0
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-06-07 14:43:25 -0300

    refactor: handle messages correctly

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
-  try {
-    const user = await signUp(formData);
-    return NextResponse.json({ success: true, user });
-  } catch (error: unknown) {
-    let message = "An unknown error occurred";
-    if (error instanceof Error) {
-      message = error.message;
-    }
-    return NextResponse.json({ success: false, message }, { status: 400 });
+  const result = await signUp(formData);
+
+  if (!result.success) {
+    return NextResponse.json(
+      { success: false, message: result.message },
+      { status: 400 }
+    );
   }
+
+  return NextResponse.json({ success: true, user: result.data });
 }
