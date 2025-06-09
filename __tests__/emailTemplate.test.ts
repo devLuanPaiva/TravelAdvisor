3d929edb8ae697441df52845bbfe467600a57577
Luan <devluanpaiva@gmail.com>
2025-06-09 08:30:07 -0300

    test: add test to send and email with basic text

---
+++ __tests__/emailTemplate.test.ts
@@ -0,0 +1,36 @@
+import { emailTemplate } from "../src/functions";
+import { resend } from "../src/lib/email/resend";
+
+jest.mock("../src/lib/email/resend", () => ({
+    resend: {
+        emails: {
+            send: jest.fn(),
+        },
+    },
+}));
+
+describe("emailTemplate", () => {
+    const mockedSend = resend.emails.send as jest.Mock;
+
+    beforeEach(() => {
+        mockedSend.mockClear();
+    });
+
+    it("must send an email with basic text", async () => {
+        await emailTemplate({
+            email: "test@example.com",
+            name: "João",
+            subject: "Assunto de Teste",
+            text1: "Texto principal",
+        });
+
+        expect(mockedSend).toHaveBeenCalledTimes(1);
+        expect(mockedSend).toHaveBeenCalledWith(
+            expect.objectContaining({
+                to: "test@example.com",
+                subject: "Assunto de Teste",
+                html: expect.stringContaining("Olá, João!"),
+            })
+        );
+    });
+})
