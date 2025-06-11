e10cdc7b706aa00c666f742448a83f81e8fabfcb
Luan <devluanpaiva@gmail.com>
2025-06-11 08:07:34 -0300

    style: adapter content position

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -4,12 +4,14 @@ import { Logo } from "../shared/Logo";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between bg-gray-900  text-white h-[10vh] px-8 py-6 gap-5">
+    <header className="flex flex-col sm:flex-row items-center sm:justify-between bg-gray-900  text-white sm:h-[10vh] px-8 py-6 sm:gap-5">
       <Logo />
-      <nav className="flex-1 flex items-center justify-end ">
-        <Link href="/planos">Nossos planos</Link>
-      </nav>
-      <CurrentCity />
+      <div className="flex flex-1 gap-3 max-sm:justify-between max-sm:w-full">
+        <nav className="flex-1 flex items-center sm:justify-end ">
+          <Link href="/planos">Nossos planos</Link>
+        </nav>
+        <CurrentCity />
+      </div>
     </header>
   );
 }
