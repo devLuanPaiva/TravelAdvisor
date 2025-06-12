9a3e23063f58c199ac75784685cd275c166307df
Luan <devluanpaiva@gmail.com>
2025-06-12 08:42:58 -0300

    refactor: remove PlansSearch component

--- src/app/(pages)/planos/page.tsx
+++ src/app/(pages)/planos/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { Plans } from "@/components/plans/Plans";
-import { PlansSearch } from "@/components/plans/PlansSearch";
 
 export default async function PlansPage() {
   const session = await getServerSession();
@@ -10,7 +9,6 @@ export default async function PlansPage() {
   }
   return (
     <div className="flex flex-col items-center justify-start w-full py-10">
-      <PlansSearch />
       <Plans session={session} />
     </div>
   );
